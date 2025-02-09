47b9915a0e4172de574a7ccf69298f27050a1d75
Erick Ruiz <3934502+erickruiz29@users.noreply.github.com>
2025-02-09 14:25:48 -0800

    add functionality

--- src/ColorSelector.tsx
+++ src/ColorSelector.tsx
@@ -1,17 +1,27 @@
 'use strict'
 
 import { useEffect, useState } from 'react';
-import CopyIcon from './copy-icon.svg?react'
-import Checkmark from './checkmark.svg?react'
+import CopyIcon from './copy-icon.svg?react';
+import Checkmark from './checkmark.svg?react';
+import SaveIcon from './save-icon.svg?react';
+import LoadIcon from './load-icon.svg?react';
+import DownloadIcon from './download-icon.svg?react';
+import DeleteIcon from './delete-icon.svg?react';
 import chroma from 'chroma-js';
 import { ChromePicker, ColorResult } from 'react-color';
 
+interface SavedColors {
+    colors: string[];
+    location: string[];
+    datetime: string;
+}
 
 
 const ColorSelector = () => {
     const [color, setColor] = useState('#abc123');
     const [selectedColors, setSelectedColors] = useState<string[]>([]);
     const [isCopied, setIsCopied] = useState(false);
+    const [retrievedColors, setRetrievedColors] = useState<SavedColors[]>([]);
 
     const handleChange = (color: ColorResult) => {
         setColor(color.hex);
@@ -61,6 +71,7 @@ const ColorSelector = () => {
         window.addEventListener('resize', handleOrientationChange);
         handleOrientationChange();
         getLocation();
+        setRetrievedColors(JSON.parse(localStorage.getItem('colorsStorage') ?? "[]"));
         return () => {
             window.removeEventListener('resize', handleOrientationChange);
         };
@@ -69,17 +80,13 @@ const ColorSelector = () => {
     return (
         <div style={{ display: 'flex', flexDirection: isLandscape ? 'row' : 'column', alignItems: 'center' }}>
             <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', padding: '1rem' }}>
-                <div onClick={() => copyToClipboard(selectedColors)} style={{ cursor: 'pointer', marginRight: '1rem' }}>
-                    {(isCopied ? <Checkmark style={{ width: '50px', height: '50px', position: 'absolute', top: '50px', left: '50px' }} /> :
-                        <CopyIcon style={{ width: '50px', height: '50px', position: 'absolute', top: '50px', left: '50px' }} />)}
-                </div>
                 <div style={{ width: '100px', height: '100px', backgroundColor: color }} onClick={() => { selectedColors.push(color); setSelectedColors([...selectedColors]); }}></div>
             </div>
             <div style={{ display: 'flex', flexDirection: isLandscape ? 'column' : 'row', alignItems: 'center', flexWrap: 'nowrap', width: isLandscape ? '100px' : '100%', height: isLandscape ? '90vh' : '100px', padding: '1rem' }}>
-                {(selectedColors).map((curColor) => {
+                {(selectedColors).map((curColor, i) => {
                     const textColor = (chroma.contrast('#000000', curColor) > chroma.contrast('#ffffff', curColor) ? '#000000' : '#ffffff');
                     return (
-                        <div key={curColor} style={{
+                        <div key={curColor + i} style={{
                             minWidth: '20px',
                             width: 'inherit',
                             maxWidth: '100px',
@@ -102,6 +109,51 @@ const ColorSelector = () => {
                 onChange={handleChange}
                 styles={{ default: { picker: { width: '100%', maxWidth: '500px' } } }}
             />
+            <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', padding: '1rem' }}>
+                <div onClick={() => copyToClipboard(selectedColors)} style={{ cursor: 'pointer' }}>
+                    {(isCopied ? <Checkmark style={{ width: '50px', height: '50px' }} /> :
+                        <CopyIcon style={{ width: '50px', height: '50px' }} />)}
+                </div>
+                <div onClick={() => {
+                    const retrievedColors: SavedColors[] = JSON.parse(localStorage.getItem('colorsStorage') ?? "[]");
+                    const datetime = new Date().toLocaleString();
+                    const loc = location ? `(${location.latitude}, ${location.longitude})` : '';
+                    retrievedColors.push({ colors: selectedColors, location: [loc], datetime });
+                    localStorage.setItem('colorsStorage', JSON.stringify(retrievedColors));
+                    setRetrievedColors(JSON.parse(localStorage.getItem('colorsStorage') ?? "[]"));
+                }
+                } style={{ cursor: 'pointer', marginLeft: '1rem' }}>
+                    <SaveIcon style={{ width: '50px', height: '50px', }} />
+                </div>
+                <div onClick={() => {
+                    const lastColor = retrievedColors.pop() ?? { colors: [] };
+                    setSelectedColors(lastColor.colors);
+                }} style={{ cursor: 'pointer', marginLeft: '1rem' }}>
+                    <LoadIcon style={{ width: '50px', height: '50px', }} />
+                </div>
+                <div onClick={() => {
+                    setRetrievedColors(JSON.parse(localStorage.getItem('colorsStorage') ?? "[]"));
+                    const datetime = new Date().toLocaleString();
+                    const element = document.createElement("a");
+                    const file = new Blob([JSON.stringify(retrievedColors, null, 2)], { type: 'application/json' });
+                    element.href = URL.createObjectURL(file);
+                    const sanitizedDatetime = datetime.replace(/\s+/g, '');
+                    element.download = `colors-${sanitizedDatetime}.json`;
+                    document.body.appendChild(element);
+                    element.click();
+                    document.body.removeChild(element);
+                }} style={{ cursor: 'pointer', marginLeft: '1rem' }}>
+                    <DownloadIcon style={{ width: '50px', height: '50px', }} />
+                </div>
+                <div onClick={() => {
+                    if (window.confirm('Are you sure you want to delete all saved colors?')) {
+                        localStorage.setItem('colorsStorage', JSON.stringify([]));
+                        setRetrievedColors(JSON.parse(localStorage.getItem('colorsStorage') ?? "[]"));
+                    }
+                }} style={{ cursor: 'pointer', marginLeft: '1rem' }}>
+                    <DeleteIcon style={{ width: '50px', height: '50px', }} />
+                </div>
+            </div>
         </div >
     );
 };
