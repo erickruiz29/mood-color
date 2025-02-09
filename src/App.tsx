2fe869e8216fc7d2fb5037c0dd3ab72e7ee39155
Erick Ruiz <3934502+erickruiz29@users.noreply.github.com>
2025-02-09 13:03:54 -0800

    touch fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
+import { useEffect } from 'react'
 import './App.css'
 import ColorSelector from './ColorSelector'
 
 function App() {
+  useEffect(() => {
+    document.body.addEventListener("touchmove", function (e) {
+      e.preventDefault();
+    });
+    document.body.addEventListener("scroll", function (e) {
+      e.preventDefault();
+    });
+    document.body.addEventListener("wheel", function (e) {
+      e.preventDefault();
+    }, false);
+  }, [])
   return (
     <>
       <ColorSelector />
