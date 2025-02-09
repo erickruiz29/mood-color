d0b4074c413a79eff0612bf7b147df2662eb5bc5
Erick Ruiz <3934502+erickruiz29@users.noreply.github.com>
2025-02-09 13:48:55 -0800

    add copy

--- src/ColorSelector.tsx
+++ src/ColorSelector.tsx
@@ -1,20 +1,33 @@
 'use strict'
 
 import { useEffect, useState } from 'react';
+import CopyIcon from './copy-icon.svg?react'
+import Checkmark from './checkmark.svg?react'
 import chroma from 'chroma-js';
 import { ChromePicker, ColorResult } from 'react-color';
+
+
 
 const ColorSelector = () => {
     const [color, setColor] = useState('#abc123');
     const [selectedColors, setSelectedColors] = useState<string[]>([]);
+    const [isCopied, setIsCopied] = useState(false);
 
     const handleChange = (color: ColorResult) => {
         setColor(color.hex);
     };
 
-
-
     const [isLandscape, setIsLandscape] = useState(false);
+
+    const copyToClipboard = (colors: string[]) => {
+        const textToCopy = colors.join(', ');
+        navigator.clipboard.writeText(textToCopy).then(() => {
+            setIsCopied(true);
+            setTimeout(() => setIsCopied(false), 1000);
+        }).catch(err => {
+            console.error('Failed to copy: ', err);
+        });
+    };
 
     useEffect(() => {
         const handleOrientationChange = () => {
@@ -36,6 +49,10 @@ const ColorSelector = () => {
     return (
         <div style={{ display: 'flex', flexDirection: isLandscape ? 'row' : 'column', alignItems: 'center' }}>
             <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', padding: '1rem' }}>
+                <div onClick={() => copyToClipboard(selectedColors)} style={{ cursor: 'pointer', marginRight: '1rem' }}>
+                    {(isCopied ? <Checkmark style={{ width: '50px', height: '50px', position: 'absolute', top: '50px', left: '50px' }} /> :
+                        <CopyIcon style={{ width: '50px', height: '50px', position: 'absolute', top: '50px', left: '50px' }} />)}
+                </div>
                 <div style={{ width: '100px', height: '100px', backgroundColor: color }} onClick={() => { selectedColors.push(color); setSelectedColors([...selectedColors]); }}></div>
             </div>
             <div style={{ display: 'flex', flexDirection: isLandscape ? 'column' : 'row', alignItems: 'center', flexWrap: 'nowrap', width: isLandscape ? '100px' : '100%', height: isLandscape ? '90vh' : '100px', padding: '1rem' }}>
@@ -63,7 +80,7 @@ const ColorSelector = () => {
             <ChromePicker
                 color={color}
                 onChange={handleChange}
-                styles={{ default: { picker: { width: '100%', maxWidth: '500px' }} }}
+                styles={{ default: { picker: { width: '100%', maxWidth: '500px' } } }}
             />
         </div >
     );
